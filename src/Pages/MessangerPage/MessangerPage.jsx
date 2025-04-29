486cc6dd1f08c665f94481ac26038b16361c5d10
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-04-29 17:23:41 +0600

    deploy server to railway and made url change

--- src/Pages/MessangerPage/MessangerPage.jsx
+++ src/Pages/MessangerPage/MessangerPage.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import LayoutMassenger from '../../Components/Messenger/LayoutMassenger/LayoutMassenger';
+import React from "react";
 
 const MessangerPage = () => {
-    return (
-        <div>
-            {/* <LayoutMassenger></LayoutMassenger> */}
-
-            <h1>Comming Soon.....</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Comming Soon.....</h1>
+    </div>
+  );
 };
 
 export default MessangerPage;
